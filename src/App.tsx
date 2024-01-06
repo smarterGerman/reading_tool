5f9a7f2663a1e5fc6a5bcb42cfed90931e581f4e
Roman Franchuk <franchukrom@gmail.com>
2024-01-06 16:21:04 +0100

    Add TTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import './App.css'
 import Feedback from './Feedback.tsx'
@@ -9,7 +9,25 @@ function App() {
     "Die Sonne scheint und die Vögel zwitschern fröhlich in den Bäumen.",
   ]
 
-  const [sentenceIndex, setSentenceIndex] = useState(0)
+  // Special values:
+  // -1 - unstarted
+  // sentences.length - finished
+  const [sentenceIndex, setSentenceIndex] = useState(-1)
+
+  function speakCurrentSentence() {
+    if(sentenceIndex >= 0 && sentenceIndex < sentences.length) {
+      speechSynthesis.cancel()
+      let utterance = new SpeechSynthesisUtterance(sentences[sentenceIndex])
+      utterance.lang = "de_DE"
+      utterance.onerror = (e: any) => {
+        console.log("TTS error: " + e.error)
+        console.log("Message: " + e.message)
+      }
+      speechSynthesis.speak(utterance)
+    }
+  }
+
+  useEffect(speakCurrentSentence, [sentenceIndex])
 
   const {
     transcript,
@@ -36,20 +54,24 @@ function App() {
     }
   }
 
-  function nextSentence() {
+  function goToSentence(index: number) {
     if (listening) {
       SpeechRecognition.stopListening()
     }
     resetTranscript()
-    setSentenceIndex(sentenceIndex + 1)
+    setSentenceIndex(index)
+  }
+
+  function nextSentence() {
+    goToSentence(sentenceIndex + 1)
   }
 
   function prevSentence() {
-    if (listening) {
-      SpeechRecognition.stopListening()
-    }
-    resetTranscript()
-    setSentenceIndex(sentenceIndex - 1)
+    goToSentence(sentenceIndex - 1)
+  }
+
+  if(sentenceIndex < 0) {
+    return <button onClick={nextSentence}>Start</button>
   }
 
   if(sentenceIndex === sentences.length) {
@@ -62,7 +84,7 @@ function App() {
 
   return (
     <>
-      <p>{sentences[sentenceIndex]}</p>
+      <p>{sentences[sentenceIndex]}<button onClick={speakCurrentSentence}>🔊</button></p>
       <Feedback transcript={transcript} sentence={sentences[sentenceIndex]} listening={listening} />
       <button onClick={toggleSpeechRecognition}>🎤 {listening ? "Listening..." : ""}</button>
       { sentenceIndex > 0 ? <button onClick={prevSentence}>Back</button> : null}
