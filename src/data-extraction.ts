1921f4f44e9cf647babf47f89e39bbb636328a14
smartergerman <michael@smartergerman.com>
2026-01-28 15:40:57 +0100

    Fix truncated data-extraction.ts file

--- src/data-extraction.ts
+++ src/data-extraction.ts
@@ -29,4 +29,17 @@ type SentenceData = {
     sections: Section[]
 }
 
+/**
+ * Takes JSON data and parses it into sentences.
+ * @param data JSON dictation data
+ * @param id lesson id
+ * @returns an array of strings representing sentences
+ */
 export function extractSentences(data: SentenceData, id: string) {
+    for (const section of data.sections) {
+        if (section.id === id) {
+            return extractSentencesFromSection(section)
+        }
+    }
+    return null
+}
